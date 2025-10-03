d22c22d1a27ce82a4f7ca9407078b55b56aa95fa
thedayisuicide <thedayisuicide@gmail.com>
2025-10-03 15:01:07 +0700

    Implement audio bus system and expand options UI
    Edit Audio Manager: Volume setting now decide by Audio Bus.
    
    UI Option add:
    resolution,
    3d scaling, vsync, volume

--- jamsepticeye-gj/scripts/audio_client.gd
+++ jamsepticeye-gj/scripts/audio_client.gd
@@ -2,7 +2,8 @@ extends Node3D
 
 func _ready() -> void:
 	pass
-	
+
+
 #Create stream that will free after finished
 func play_oneshot(
 		p_audio_tag: String, 
@@ -15,6 +16,7 @@ func play_oneshot(
 	new_oneshot_stream.stream = AudioManager.get_audio_by_tag(p_audio_tag).file
 	new_oneshot_stream.max_distance = p_max_distance
 	new_oneshot_stream.unit_size = p_unit_size
+	new_oneshot_stream.bus = AudioManager.BUS_SFX
 	add_child(new_oneshot_stream)
 	new_oneshot_stream.connect("finished", Callable(new_oneshot_stream, "queue_free"))
 	new_oneshot_stream.play()
@@ -33,6 +35,7 @@ func play_polyphony(
 	new_polyphony_stream.max_distance = p_max_distance
 	new_polyphony_stream.unit_size = p_unit_size
 	new_polyphony_stream.max_polyphony = p_max_polyphony
+	new_polyphony_stream.bus = AudioManager.BUS_SFX
 	add_child(new_polyphony_stream)
 	new_polyphony_stream.play()
 	return new_polyphony_stream
@@ -48,6 +51,7 @@ func play_maintain(
 	new_maintain_stream.stream = AudioManager.get_audio_by_tag(p_audio_tag).file
 	new_maintain_stream.max_distance = p_max_distance
 	new_maintain_stream.unit_size = p_unit_size
+	new_maintain_stream.bus = AudioManager.BUS_SFX
 	add_child(new_maintain_stream)
 	new_maintain_stream.connect("finished", Callable(new_maintain_stream, "play"))
 	new_maintain_stream.play()
